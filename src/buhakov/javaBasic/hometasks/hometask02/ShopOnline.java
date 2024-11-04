3630d8ded66a1d4ce66e94bba084d6c3777eb89c
VladyslavBulgakov <bulgvladd@gmal.com>
2024-11-04 14:44:59 +0200

    hometask02 - Changed string in ShopOnline

--- src/buhakov/javaBasic/hometasks/hometask02/ShopOnline.java
+++ src/buhakov/javaBasic/hometasks/hometask02/ShopOnline.java
@@ -2,7 +2,7 @@ package buhakov.javaBasic.hometasks.hometask02;
 
 public class ShopOnline {
     public static void main(String[] args) {
-        String shop = "Shop-online for customers";
+        String shop = "This is shop-online app";
         System.out.println(shop);
     }
 }
