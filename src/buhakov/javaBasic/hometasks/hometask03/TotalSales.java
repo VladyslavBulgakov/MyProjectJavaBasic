22ef5038b6bf051653be62e1e314c0b353bb1b12
VladyslavBulgakov <bulgvladd@gmal.com>
2024-11-05 09:07:21 +0200

    hometask03 - changed sout with formatting

--- src/buhakov/javaBasic/hometasks/hometask03/TotalSales.java
+++ src/buhakov/javaBasic/hometasks/hometask03/TotalSales.java
@@ -8,24 +8,23 @@ public class TotalSales {
         String productName = "smartphone";
         int days = 5;
 //        double salesByDay = 2430.68;
-        double totalSales = 12153.41;
-        double result = totalSales / days;
+        double totalSales = 12153.4;
+        double salesPerDay = totalSales / days;
 
-        System.out.printf("Product No " + productNum + ": " + productName + ",\n"
-                + "total sales for " + days + " days is " + CURRENCY + " %.2f" + ",\n"
-                + "sales by day is " + CURRENCY + " %.2f.", totalSales, result
-        );
+        System.out.printf("Product No %d: %s\n", productNum, productName);
+        System.out.printf("total sales for %s days is %.1f,\n", CURRENCY, totalSales);
+        System.out.printf("sales by day is %s %.2f.", CURRENCY, salesPerDay);
+
 
         productNum++;
         productName = "laptop";
         days = 7;
 //        salesByDay = 1498.12;
         totalSales = 10486.85;
-        result = totalSales / days;
+        salesPerDay = totalSales / days;
 
-        System.out.printf("\nProduct No " + productNum + ": " + productName + ",\n"
-                + "total sales for " + days + " days is " + CURRENCY + " %.2f" + ",\n"
-                + "sales by day is " + CURRENCY + " %.2f.", totalSales, result
-        );
+        System.out.printf("\nProduct No %d: %s\n", productNum, productName);
+        System.out.printf("total sales for %s days is %.2f,\n", CURRENCY, totalSales);
+        System.out.printf("sales by day is %s %.2f.", CURRENCY, salesPerDay);
     }
 }
