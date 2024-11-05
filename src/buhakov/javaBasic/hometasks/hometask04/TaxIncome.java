a562ecec42595ce30df81fc16e79ad9968ab0f37
VladyslavBulgakov <bulgvladd@gmal.com>
2024-11-05 16:30:27 +0200

    hometask04 - Calculating income tax

---
+++ src/buhakov/javaBasic/hometasks/hometask04/TaxIncome.java
@@ -0,0 +1,36 @@
+package buhakov.javaBasic.hometasks.hometask04;
+
+import java.util.Scanner;
+
+public class TaxIncome {
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Write your income to get an amount taxes: ");
+
+            int income = sc.nextInt();
+            double clearIncome;
+            double taxResult;
+            double lowTaxRate = 2.5;
+            double midTaxRate = 4.3;
+            double highTaxRate = 6.7;
+
+            if (income > 0 && income <= 10000) {
+                taxResult = (income * lowTaxRate) / 100;
+                clearIncome = income - taxResult;
+                System.out.println("Your tax amount of " + income + " is "+ taxResult + ".");
+                System.out.println("Your clear income is: " + clearIncome + ".");
+            } else if (income > 10001 && income <= 25000){
+                taxResult = (income * midTaxRate) / 100;
+                clearIncome = income - taxResult;
+                System.out.println("Your tax amount of " + income + " is "+ taxResult + ".");
+                System.out.println("Your clear income is: " + clearIncome + ".");
+            } else {
+                taxResult = (income * highTaxRate) / 100;
+                clearIncome = income - taxResult;
+                System.out.println("Your tax amount of " + income + " is "+ taxResult + ".");
+                System.out.println("Your clear income is: " + clearIncome + ".");
+            }
+            sc.close();
+    }
+}
